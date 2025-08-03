7ae7f95684a29e23fa177be6447f40d0812c6952
hardik <hardiksuthar74@gmail.com>
2025-08-03 10:31:56 +0530

    Initial setup done

---
+++ src/main/java/com/service/racertracker/RacertrackerApplication.java
@@ -0,0 +1,13 @@
+package com.service.racertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacertrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacertrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/racertracker/RacertrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.racertracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacertrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

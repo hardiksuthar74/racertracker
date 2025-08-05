506ca77d6118a7d2140aee751226e62910e65f2c
hardik <hardiksuthar74@gmail.com>
2025-08-05 21:33:37 +0530

    feat: add email, user service and user repository

---
+++ src/main/java/com/service/racertracker/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.service.racertracker.repository;
+
+import com.service.racertracker.db.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+}
